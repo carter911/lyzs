195c3bfb3015e7047b7d1741761f4f29ae9d4217
carter911 <729921697@qq.com>
2019-08-18 09:43:12 +0800

    更新工地直播

--- application/index/controller/Cron.php
+++ application/index/controller/Cron.php
@@ -22,6 +22,77 @@ class Cron extends Controller
 		//Db::name('project')->where('id','gt',0)->delete();die;
 		$time = date("Y-m-d 00:00:00", time());
 		try {
+			$id = Db::name('project')->order('sgb_id desc')->field('sgb_id')->find();
+			echo Db::name('project')->getLastSql().'</br>';
+			echo '更新项目详情'.var_export($id,true).'<br/>';
+			$id = empty($id) ? 0 : $id['sgb_id'];
+			//初始化
+			$curl = curl_init();
+			//设置抓取的url
+			curl_setopt($curl, CURLOPT_URL, 'http://api.e-shigong.com/serverProjectList?companyId=44&start=' . $id);
+			//设置头文件的信息作为数据流输出
+			curl_setopt($curl, CURLOPT_HEADER, 1);
+			//设置获取的信息以文件流的形式返回，而不是直接输出。
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curl, CURLOPT_HEADER, false);
+			//执行命令
+			$data = curl_exec($curl);
+			//关闭URL请求
+			curl_close($curl);
+			//显示获得的数据
+			$data = json_decode($data, true);
+
+			if (empty($data['data'])) {
+				echo '暂无数据';
+				die;
+			}
+
+			foreach ($data['data'] as $key => $val) {
+				$city['name'] = '';
+				if (!empty($val['city'])) {
+					$city = Db::name('area')->where(['code' => $val['city']])->find();
+				}
+				echo '同步施公宝工地[' . $val['name'] . $val['circle_name'] . ']</br>';
+					echo '添加';
+					$arr   = [
+						'sgb_id'       => $val['id'],
+						'name'         => $val['name'],
+						'city'         => $val['city'],
+						'city_name'    => $city['name'],
+						'start_time'   => date("Y-m-d H:i:s", $val['start_time']),
+						'end_time'     => date("Y-m-d H:i:s", $val['end_time']),
+						'circle'       => json_encode($val['circle']),
+						'task'         => json_encode($val['task']),
+						'image'        => json_encode($val['images_list']),
+						'circle_name'  => $val['circle_name'],
+						'circle_id'    => $val['circle_id'],
+						//				'type'=>$val['id'],
+						//				'area'=>$val['id'],
+						'look_num'     => rand(1, 100),
+						'project_time' => $val['create_time'],
+						'create_time'  => date("Y-m-d H:i:s", time()),
+						'update_time'  => date("Y-m-d H:i:s", time()),
+					];
+					$add[] = $arr;
+
+			}
+
+			if (!empty($add)) {
+				Db::name('project')->insertAll($add);
+			}
+		} catch (Exception $e) {
+			print_r($e);
+		}
+	}
+
+
+
+
+	public function update_project()
+	{
+		//Db::name('project')->where('id','gt',0)->delete();die;
+		$time = date("Y-m-d 00:00:00", time());
+		try {
 			$id = Db::name('project')->where('update_time','lt',$time)->order('update_time asc')->field('sgb_id,id')->find();
 			echo Db::name('project')->getLastSql().'</br>';
 			echo '更新项目详情'.var_export($id,true).'<br/>';
